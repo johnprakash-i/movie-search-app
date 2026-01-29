ac3683c78db736ae9f903660c203e86732bbc58b
johnprakash-i <viratjohn110@gmail.com>
2026-01-29 18:10:28 +0530

    Fix:Build error fixed

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,5 +1,5 @@
 import type { Movie } from "../types/movie";
-import { Link, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 import { useAppContext } from "../context/AppContext";
 
 interface Props {
